72a4a05582ad81cab9c454afbcdb55975285cb3d
Justin Diaz <justin.s.diaz@gmail.com>
2021-09-27 19:01:49 -0400

    created login page

---
+++ src/components/SignIn/styles/SignInForm.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+min-height:65rem;
+display:flex;
+justify-content: center;
+
+`
+
+export const Pane = styled.div`
+
+
+    `
